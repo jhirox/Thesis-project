0bf1c4c22aec1bffbe1d6615cf25e6d6157be684
jhirox <lj6299389@gmail.com>
2026-03-01 15:15:19 +0800

    3:15 feb 2026

---
+++ data/user.js
@@ -0,0 +1,10 @@
+export const user_data = [
+    {username: "admin@gmail.com", password: "P@ssw0rd", fullname: "Administrator"},
+    {username: "nathaniel@gmail.com", password: "P@ssw0rd", fullname: "Nathaniel Amandy"},
+    {username: "yuri@gmail.com", password: "P@ssw0rd", fullname: "Yuri Oflaria"},
+    {username: "zairus@gmail.com", password: "P@ssw0rd", fullname: "Zairus Jester Gilles"},
+    {username: "lance@gmail.com", password: "P@ssw0rd", fullname: "Lance Jimenez"},
+    {username: "john@gmail.com", password: "P@ssw0rd", fullname: "John Zandrex Saniel"},
+    {username: "gerick@gmail.com", password: "P@ssw0rd", fullname: "Gerick Ocampo"},
+    {username: "mark@gmail.com", password: "P@ssw0rd", fullname: "Mark Kenneth Delacruz"},
+]
