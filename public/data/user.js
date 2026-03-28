e1a87dc09da3630fba749623fa32587942cbbb17
jhirox <lj6299389@gmail.com>
2026-03-28 14:24:21 +0800

    Re-fixing login in home page

--- public/data/user.js
+++ public/data/user.js
@@ -1,10 +1,11 @@
+// This is now a proper ES module
 export const user_data = [
-    { username: "admin@gmail.com", password: "P@ssw0rd", fullname: "Administrator" },
-    { username: "nathanielamandy01@gmail.com", password: "P@ssw0rd", fullname: "Nathaniel Amandy" },
-    { username: "yuri@gmail.com", password: "P@ssw0rd", fullname: "Yuri Oflaria" },
-    { username: "zairus@gmail.com", password: "P@ssw0rd", fullname: "Zairus Jester Gilles" },
-    { username: "lance@gmail.com", password: "P@ssw0rd", fullname: "Lance Jimenez" },
-    { username: "john@gmail.com", password: "P@ssw0rd", fullname: "John Zandrex Saniel" },
-    { username: "gerick@gmail.com", password: "P@ssw0rd", fullname: "Gerick Ocampo" },
-    { username: "registrar@gmail.com", password: "P@ssw0rd", fullname: "Registrar User" },
+  { username: "admin@gmail.com", password: "P@ssw0rd", fullname: "Administrator" },
+  { username: "nathanielamandy01@gmail.com", password: "P@ssw0rd", fullname: "Nathaniel Amandy" },
+  { username: "yuri@gmail.com", password: "P@ssw0rd", fullname: "Yuri Oflaria" },
+  { username: "zairus@gmail.com", password: "P@ssw0rd", fullname: "Zairus Jester Gilles" },
+  { username: "lance@gmail.com", password: "P@ssw0rd", fullname: "Lance Jimenez" },
+  { username: "john@gmail.com", password: "P@ssw0rd", fullname: "John Zandrex Saniel" },
+  { username: "gerick@gmail.com", password: "P@ssw0rd", fullname: "Gerick Ocampo" },
+  { username: "registrar@gmail.com", password: "P@ssw0rd", fullname: "Registrar User" },
 ];
