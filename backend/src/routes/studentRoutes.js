8d92471f88bd8f1cecdf871435db541ef12b9d70
jhirox <lj6299389@gmail.com>
2026-04-11 02:56:21 +0800

    fixing enrollment-form connection to database

--- backend/src/routes/studentRoutes.js
+++ backend/src/routes/studentRoutes.js
@@ -6,13 +6,13 @@ const router = express.Router();
 // GET all students
 router.get('/', getStudents);
 
-// GET student by ID
-router.get('/:id', getStudentByID);
-
 // GET all enrollments
 router.get('/enrollments/all', getEnrollments);
 
 // POST submit enrollment
 router.post('/enrollment', submitEnrollment);
+
+// GET student by ID
+router.get('/:id', getStudentByID);
 
 export default router;
